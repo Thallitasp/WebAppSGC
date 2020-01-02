d84716a4704a894fa1b4e0c1923a66f05dab18b2
Thallita Pedreira <thallitaspedreira@gmail.com>
2020-01-02 13:58:37 -0300

    Profissão

---
+++ SGC/src/SGC.ApplicationCore/Entity/ProfissaoCliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.ApplicationCore.Entity
+{
+    public class ProfissaoCliente
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente{ get; set; }
+        public Profissao Profissao { get; set; }
+    }
+}
