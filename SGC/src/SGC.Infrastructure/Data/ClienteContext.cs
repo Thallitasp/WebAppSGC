3960d540f9b32363226db55b087fece03432f8f7
Thallita Pedreira <thallitaspedreira@gmail.com>
2020-01-03 10:14:50 -0300

    try

--- SGC/src/SGC.Infrastructure/Data/ClienteContext.cs
+++ SGC/src/SGC.Infrastructure/Data/ClienteContext.cs
@@ -19,8 +19,20 @@ namespace SGC.Infrastructure.Data
         {
             modelBuilder.Entity<Cliente>().ToTable("Cliente");
             modelBuilder.Entity<Contato>().ToTable("Contato");
+            modelBuilder.Entity<Endereco>().ToTable("Endereco");
+            modelBuilder.Entity<Profissao>().ToTable("Profissao");
+            modelBuilder.Entity<ProfissaoCliente>().ToTable("ProfissaoCliente");
 
             #region Configurações de Cliente
+            modelBuilder.Entity<Cliente>()
+                .HasKey(c => c.ClienteId);
+
+            modelBuilder.Entity<Cliente>()
+                .HasMany(c => c.Contatos)
+                .WithOne(c => c.Cliente)
+                .HasForeignKey(c => c.ClienteId)
+                .HasPrincipalKey(c => c.ClienteId);
+
             modelBuilder.Entity<Cliente>().Property(e => e.Cpf)
                 .HasColumnType("varchar(11)")
                 .IsRequired();
@@ -34,6 +46,12 @@ namespace SGC.Infrastructure.Data
             modelBuilder.Entity<Contato>().Property(e => e.Nome)
                 .HasColumnType("varchar(200)")
                 .IsRequired();
+            modelBuilder.Entity<Contato>()
+                .HasOne(c => c.Cliente)
+                .WithMany(c => c.Contatos)
+                .HasForeignKey(c => c.ClienteId)
+                .HasPrincipalKey(c => c.ClienteId);
+
 
             modelBuilder.Entity<Contato>().Property(e => e.Email)
                 .HasColumnType("varchar(100)")
@@ -41,6 +59,38 @@ namespace SGC.Infrastructure.Data
 
             modelBuilder.Entity<Contato>().Property(e => e.Telefone)
                 .HasColumnType("varchar(15)");
+            #endregion
+
+            #region Configurações de Profissao
+            modelBuilder.Entity<Profissao>().Property(p => p.Nome)
+                .HasColumnType("varchar(400)")
+                .IsRequired();
+
+            modelBuilder.Entity<Profissao>().Property(p => p.CBO)
+                .HasColumnType("varchar(10)")
+                .IsRequired();
+
+            modelBuilder.Entity<Profissao>().Property(p => p.Descricao)
+                .HasColumnType("varchar(1000)")
+                .IsRequired();
+            #endregion
+
+            #region Configurações de Endereco
+            modelBuilder.Entity<Endereco>().Property(end => end.Bairro)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+
+            modelBuilder.Entity<Endereco>().Property(end => end.Cep)
+                .HasColumnType("varchar(15)")
+                .IsRequired();
+
+            modelBuilder.Entity<Endereco>().Property(end => end.Logradouro)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+
+            modelBuilder.Entity<Endereco>().Property(end => end.Referencia)
+                .HasColumnType("varchar(400)");
+
             #endregion
         }
     }
